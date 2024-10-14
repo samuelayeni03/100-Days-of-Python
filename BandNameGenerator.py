d82ff4e7bedb35c9c3bf895aa6b311e26709003e
Ayenitaju Samuel <110955088+samuelayeni03@users.noreply.github.com>
2024-10-14 03:23:59 +0100

    Add files via upload

---
+++ BandNameGenerator.py
@@ -0,0 +1,14 @@
+#1 create a greeting for your program...
+print('Hello user, This is a band name generator.')
+
+#2 ask the user for the city they grew up in...
+print('What city did you grow up in: ')
+city =  input()
+
+#3 ask the user for the name of a per...
+print('What is the name of your pet: ')
+pet = input()
+
+#4 combine the name of their city and pet and show their band name...
+print('Your band name is ' + city +  " " + pet)
+
---
+++ READme.txt
@@ -0,0 +1,1 @@
+This is the solution to all the final projects in the 100 Days of Code Challenge by Angela Wu
